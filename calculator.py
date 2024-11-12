19b11cc09620b457348cf736f9126979cb293a77
Marcel Suter <marcel.suter@bzz.ch>
2024-11-12 07:08:32 +0100

    initial commit

---
+++ calculator.py
@@ -0,0 +1,8 @@
+def main():
+    divide(6,4)
+
+def divide(a, b):
+    return a / b
+
+if __name__ == '__main__':
+    main()
---
+++ test_main.py
@@ -0,0 +1,36 @@
+def test_success():
+    # TODO: divide two numbers
+    # TODO: assert correct result using pytext.approx
+    pass
+
+
+def test_invalid():
+    # TODO: call divide with invalid input (letters, symbols, ...)
+    # TODO: use xfails to mark the test as expected to fail
+    pass
+
+
+def test_zero_division():
+    # TODO: call divide with zero as second parameter
+    # TODO: use pytest.raises to check for ZeroDivisionError
+    pass
+
+
+def test_mark_skip():
+    # TODO: mark this test as skipped using a decorator
+    # TODO: provide a reason for skipping
+    assert False
+
+
+def test_mark_skip_conditional():
+    # TODO: skip this test using a decorator if the Python version is greater than 3.8
+    # TODO: provide a reason for skipping
+    assert False
+
+
+def test_pytest_skip():
+    if True:
+        # TODO: mark this test as skipped using pytest.skip
+        # TODO: provide a reason for skipping
+        pass
+    assert False
